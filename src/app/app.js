14f1d3a160b6f7386a975eaca228e6a771e0f502
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-13 16:46:31 +0000

    Added message and messagelist components

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class SimpleComponent extends React.Component {
-	render(){
-		return (
-			<div>{this.props.message}</div>
-		);
-	}
-}
+import MessageList from './components/MessageList';
+
+let messages = [
+	{
+        id: 1, 
+        timestamp: 1478859071000,
+        owner: 'Chuck',
+        text: 'Hello Mark, how is it going?' 
+    },
+    {
+        id: 2,
+        timestamp: 1478859115000,
+        owner: 'Mark',
+        text: 'What do you want, Chuck?'
+    },
+    {
+        id: 3,
+        timestamp: 1478859131000,
+        owner: 'Chuck',
+        text: ':D'
+    },
+    {
+        id: 4,
+        timestamp: 1478859165000,
+        owner: 'Chuck',
+        text: 'I forgot you were not into small talk.'
+    }
+];
 
 ReactDOM.render(
-	<SimpleComponent message="Hello, World"/>,
+	<MessageList messages={messages} />,
 	document.querySelector('.js-app')
 );
