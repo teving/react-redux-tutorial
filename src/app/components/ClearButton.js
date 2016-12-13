8e8aec4da15d1eac7eb98eba847388e9ab2f572e
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-13 17:06:59 +0000

    Moved button into its own component

---
+++ src/app/components/ClearButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class ClearButton extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			isClearDisabled: false
+		};
+	}
+
+	handleClearMessages(){
+		this.setState({
+			isClearDisabled: true
+		});
+	}
+
+	render(){
+		let button = <button onClick={this.handleClearMessages.bind(this)}>Clear</button>;
+		if (this.state.isClearDisabled){
+			button = <button disabled="disabled">Clear</button>;
+		}
+
+		return <div>{button}</div>;
+	}
+}
+
+export default ClearButton;
--- src/app/components/MessageList.js
+++ src/app/components/MessageList.js
@@ -2,29 +2,10 @@ import React from 'react';
 import Message from './Message';
 
 class MessageList extends React.Component {
-	constructor(props, context){
-		super(props, context);
-		this.state = {
-			isClearDisabled: false
-		};
-
-		this.handleClearMessages = this.handleClearMessages.bind(this);
-	}
-
-	handleClearMessages(){
-		this.setState({
-			isClearDisabled: true
-		});
-	}
-
 	render(){
-		let button = <button onClick={this.handleClearMessages}>Clear</button>;
-		if (this.state.isClearDisabled){
-			button = <button disabled="disabled">Clear</button>;
-		}
 		return (
 			<div>
-				{this.state.isClearDisabled ? [] : 
+				{
 					this.props.messages.map(message => 
 						<Message timestamp={message.timestamp}
 									owner={message.owner}
@@ -32,7 +13,6 @@ class MessageList extends React.Component {
 									key={message.id} />
 					)
 				}
-				{button}
 			</div>
 		);
 	}
