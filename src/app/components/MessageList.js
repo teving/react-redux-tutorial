0dd654713b84174457651855096188e51081d27b
Kevin Gurton <kevin.gurton@gmail.com>
2016-12-13 16:56:48 +0000

    Implemented clear button

--- src/app/components/MessageList.js
+++ src/app/components/MessageList.js
@@ -2,15 +2,37 @@ import React from 'react';
 import Message from './Message';
 
 class MessageList extends React.Component {
+	constructor(props, context){
+		super(props, context);
+		this.state = {
+			isClearDisabled: false
+		};
+
+		this.handleClearMessages = this.handleClearMessages.bind(this);
+	}
+
+	handleClearMessages(){
+		this.setState({
+			isClearDisabled: true
+		});
+	}
+
 	render(){
+		let button = <button onClick={this.handleClearMessages}>Clear</button>;
+		if (this.state.isClearDisabled){
+			button = <button disabled="disabled">Clear</button>;
+		}
 		return (
 			<div>
-				{this.props.messages.map(message => 
-					<Message timestamp={message.timestamp}
-								owner={message.owner}
-								text={message.text}
-								key={message.key} />
-				)}
+				{this.state.isClearDisabled ? [] : 
+					this.props.messages.map(message => 
+						<Message timestamp={message.timestamp}
+									owner={message.owner}
+									text={message.text}
+									key={message.id} />
+					)
+				}
+				{button}
 			</div>
 		);
 	}
